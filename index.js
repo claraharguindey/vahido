996d65194072ffe9e17fa48a4636dd9d72ed9789
claraharguindey <clarisaharguindey@gmail.com>
2022-04-30 16:36:54 +0200

    Reset permission

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
 const iridiscenceEl = document.getElementById('iridiscence');
-function getOrientation() {
-  window.addEventListener('deviceorientation', (ev) => {
-    const alpha = ev.alpha;
-    const beta = ev.beta;
-    const gamma = ev.gamma;
 
-    if (beta > 10 && beta < 20) {
-      iridiscenceEl.className = 'pink';
-    } else if (beta < 0 && gamma > 0) {
-      iridiscenceEl.className = 'red';
-    } else if (beta < 0 && gamma < 0) {
-      iridiscenceEl.className = 'aqua';
-    } else if (beta > 0 && gamma > 0) {
-      iridiscenceEl.className = 'orange';
-    } else if (beta > 0 && gamma < 0) {
-      iridiscenceEl.className = 'yellow';
-    }
+DeviceOrientationEvent.requestPermission()
+  .then((response) => {
+    if (response == 'granted') {
+      window.addEventListener('deviceorientation', (ev) => {
+        const alpha = ev.alpha;
+        const beta = ev.beta;
+        const gamma = ev.gamma;
 
-    iridiscenceEl.innerHTML = `
+        if (beta > 10 && beta < 20) {
+          iridiscenceEl.className = 'pink';
+        } else if (beta < 0 && gamma > 0) {
+          iridiscenceEl.className = 'red';
+        } else if (beta < 0 && gamma < 0) {
+          iridiscenceEl.className = 'aqua';
+        } else if (beta > 0 && gamma > 0) {
+          iridiscenceEl.className = 'orange';
+        } else if (beta > 0 && gamma < 0) {
+          iridiscenceEl.className = 'yellow';
+        }
+
+        iridiscenceEl.innerHTML = `
        <span> beta: ${beta} </span>
        <br>
        <span> alpha: ${alpha} </span>
        <br>
        <span> gamma: ${gamma} </span>
       `;
-  });
-}
-
-getOrientation();
-
-DeviceOrientationEvent.requestPermission()
-  .then((response) => {
-    if (response == 'granted') {
-      getOrientation();
+      });
     }
   })
   .catch(console.error);
