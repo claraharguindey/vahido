613091844901f8ca7722fdc6b129d67e677b7ff4
claraharguindey <clarisaharguindey@gmail.com>
2022-04-30 16:22:57 +0200

    Remove permision

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
 const iridiscenceEl = document.getElementById('iridiscence');
 function getOrientation() {
-  DeviceOrientationEvent.requestPermission().then((response) => {
-    if (response == 'granted') {
-      window.addEventListener('deviceorientation', (ev) => {
-        const alpha = ev.alpha;
-        const beta = ev.beta;
-        const gamma = ev.gamma;
+  window.addEventListener('deviceorientation', (ev) => {
+    const alpha = ev.alpha;
+    const beta = ev.beta;
+    const gamma = ev.gamma;
 
-        if (beta > 10 && beta < 20) {
-          iridiscenceEl.className = 'pink';
-        } else if (beta < 0 && gamma > 0) {
-          iridiscenceEl.className = 'red';
-        } else if (beta < 0 && gamma < 0) {
-          iridiscenceEl.className = 'aqua';
-        } else if (beta > 0 && gamma > 0) {
-          iridiscenceEl.className = 'orange';
-        } else if (beta > 0 && gamma < 0) {
-          iridiscenceEl.className = 'yellow';
-        }
-
-        iridiscenceEl.innerHTML = `
-           <span> beta: ${beta} </span>
-           <br>
-           <span> alpha: ${alpha} </span>
-           <br>
-           <span> gamma: ${gamma} </span>
-          `;
-      });
+    if (beta > 10 && beta < 20) {
+      iridiscenceEl.className = 'pink';
+    } else if (beta < 0 && gamma > 0) {
+      iridiscenceEl.className = 'red';
+    } else if (beta < 0 && gamma < 0) {
+      iridiscenceEl.className = 'aqua';
+    } else if (beta > 0 && gamma > 0) {
+      iridiscenceEl.className = 'orange';
+    } else if (beta > 0 && gamma < 0) {
+      iridiscenceEl.className = 'yellow';
     }
+
+    iridiscenceEl.innerHTML = `
+       <span> beta: ${beta} </span>
+       <br>
+       <span> alpha: ${alpha} </span>
+       <br>
+       <span> gamma: ${gamma} </span>
+      `;
   });
 }
-
-// window.addEventListener('deviceemotion', (ev) => {
-//   console.log({
-//     acceleration: ev.acceleration.y,
-//     rotation: ev.rotationRate.gamma,
-//   });
-// });
