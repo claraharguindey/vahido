9cc66542bc8c5c97e6290103a64afa6fc11b1262
claraharguindey <clarisaharguindey@gmail.com>
2022-05-03 12:04:34 +0200

    Add permission button

--- index.js
+++ index.js
@@ -33,25 +33,27 @@ window.addEventListener('deviceemotion', (ev) => {
   });
 });
 
-DeviceOrientationEvent.requestPermission()
-.then(response => {
-  if (response == 'granted') {
-    window.addEventListener('deviceorientation', (ev) => {
-      const beta = ev.beta;
-      const gamma = ev.gamma;
-    
-      if (beta > 10 && beta < 20) {
-        iridiscenceEl.className = 'pink';
-      } else if (beta < 0 && gamma > 0) {
-        iridiscenceEl.className = 'red';
-      } else if (beta < 0 && gamma < 0) {
-        iridiscenceEl.className = 'aqua';
-      } else if (beta > 0 && gamma > 0) {
-        iridiscenceEl.className = 'orange';
-      } else if (beta > 0 && gamma < 0) {
-        iridiscenceEl.className = 'yellow';
+function getAccel(){
+  DeviceMotionEvent.requestPermission().then(response => {
+      if (response == 'granted') {
+          console.log("accelerometer permission granted");
+          window.addEventListener('deviceorientation', (ev) => {
+            const alpha = ev.alpha;
+            const beta = ev.beta;
+            const gamma = ev.gamma;
+          
+            if (beta > 10 && beta < 20) {
+              iridiscenceEl.className = 'pink';
+            } else if (beta < 0 && gamma > 0) {
+              iridiscenceEl.className = 'red';
+            } else if (beta < 0 && gamma < 0) {
+              iridiscenceEl.className = 'aqua';
+            } else if (beta > 0 && gamma > 0) {
+              iridiscenceEl.className = 'orange';
+            } else if (beta > 0 && gamma < 0) {
+              iridiscenceEl.className = 'yellow';
+            }
+          });
       }
-    })
-  }
-})
-.catch(console.error)
+  });
+}
